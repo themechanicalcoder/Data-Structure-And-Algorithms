9039dcde18c1c91a32f3a64986bf7775d0c9f34a
themechanicalcoder <34737471+themechanicalcoder@users.noreply.github.com>
2018-07-03 09:52:24 +0530

    Add files via upload

---
+++ finding_just_greater_number_in_a_range_using-segment_tree_using_vector_no_update.cpp
@@ -0,0 +1,40 @@
+//space complexity O(nlogn)
+#include<bits/stdc++.h>
+using namespace std;
+#define MAXN 100000
+int a[]={1,-1,2,5,3,7,-5,7,9,10};
+vector<int>t[4*MAXN];
+void build(int v,int tl,int tr){
+    if(tl==tr){
+        t[v]={a[tl]};
+        return ;}
+    int tm=(tl+tr)/2;
+    build(2*v,tl,tm);
+    build(2*v+1,tm+1,tr);
+    merge(t[2*v].begin(),t[2*v].end(),t[2*v+1].begin(),t[2*v+1].end(),back_inserter(t[v]));
+}
+//the answer to a query takes place in O(logn) and the entire operation is performedin O(log^2)
+int query(int v,int tl,int tr,int l,int r,int x){
+    if(l>r)
+        return INT_MAX;
+    if(l==tl && r==tr){
+        auto it=lower_bound(t[v].begin(),t[v].end(),x);
+        if(it!=t[v].end())
+            return *it;
+        return INT_MAX;}
+    int tm=(tl+tr)/2;
+    return min(
+    query(v*2,tl,tm,l,min(tm,r),x),
+    query(v*2+1,tm+1,tr,max(l,tm+1),r,x));
+
+
+}
+
+
+
+int main(){
+int n=sizeof(a)/sizeof(a[0]);
+build(1,0,n-1);
+cout<<query(1,0,n-1,1,9,6);
+return 0;
+}
